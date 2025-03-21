7a9755728675f72efa4d153fee3337f93c02ec56
MarioCab07 <00002022@uca.edu.sv>
2025-03-21 08:37:26 -0600

    Feat: add whoAmi function

--- Primavera_API_Service/routes/auth.router.js
+++ Primavera_API_Service/routes/auth.router.js
@@ -10,7 +10,7 @@ const superUserKey = process.env.Super_User_Key;
 
 
 //AUTH ROUTES
-
+//POST ROUTES
 //SuperUser route, dont use in frontend
 router.post(`/superuser/register/${superUserKey}`,authValidator.sUserRegisterValidator,validateFields, authController.superUserRegister);
 
@@ -20,12 +20,19 @@ router.post(`/login`,authController.Login);
 //User Register
 router.post('/register',authentication,authorization('modify_users'),authValidator.userRegisterValidator,validateFields,authController.userRegister);
 
+//GET ROUTES
 //Get all users
 router.get('/get/users',authentication,authorization('modify_users'),authController.getAllUsers);
 
+//Who am I
+router.get('/whoami',authentication,authController.whoAmi);
+
+//PUT ROUTES
 //Update user
 router.put('/update/user/:id',authentication,authorization('modify_users'),validateFields,authController.updateUser);
 
+
+//DELETE ROUTES
 //Delete user
 router.delete('/delete/user/:id',authentication,authorization('modify_users'),authController.deleteUser);
 
